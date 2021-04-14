471578fc71e8d1637af709be9c2f910587f1f393
noviceprogrammer <benisin5@gmail.com>
2021-04-14 13:25:02 -0400

    made tables responsive

--- stockscraper/forms.py
+++ stockscraper/forms.py
@@ -6,7 +6,7 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 class SubredditForm(forms.Form):
-    subreddit = forms.CharField(label="", max_length=200, widget=forms.TextInput(attrs={'placeholder' : 'Enter a subreddit that discusses stocks (ex: wallstreetbets, stocks, robinhood, etc.)'}), empty_value="stocks", )
+    subreddit = forms.CharField(label="", max_length=200, widget=forms.TextInput(attrs={'placeholder' : 'Enter a subreddit name'}), empty_value="stocks", )
 
     def clean_subreddit(self):
         data = self.cleaned_data['subreddit']
